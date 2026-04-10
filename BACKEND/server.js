336837e36ffcf0925c5eed08115c2e993fafb90f
Phuc <trantrongphuc1501@gmail.com>
2026-04-10 17:47:54 +0700

    khởi tạo dự án và cấu hình server.js

---
+++ BACKEND/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('UniBorrow API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
