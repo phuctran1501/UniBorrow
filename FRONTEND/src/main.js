9eecba7cdc46f391101ad4583ba58c31f62ec9fa
Phuc <trantrongphuc1501@gmail.com>
2026-04-13 19:08:18 +0700

    Hoàn thiện hệ thống đăng ký mượn sách

---
+++ FRONTEND/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia'; 
+import App from './App.vue';
+import router from './router'; 
+import VueApexCharts from "vue3-apexcharts"; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+import './style.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);     
+app.use(VueApexCharts); 
+
+app.mount('#app');
