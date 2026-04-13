b2a651a9f777ad20d1031acfbc15331123efd99e
Phuc <trantrongphuc1501@gmail.com>
2026-04-13 18:04:37 +0700

    Cập nhật các route trong server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const { initCronJobs } = require('./utils/cronJobs');
 
 const swaggerDocs = require('./config/swagger');
 
@@ -10,14 +11,13 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
-// Routes
-app.use('/api/docgia', require('./routes/docGiaRoutes'));
-app.use('/api/sach', require('./routes/sachRoutes'));
-app.use('/api/nhanvien', require('./routes/nhanVienRoutes'));
-app.use('/api/nhaxuatban', require('./routes/nhaXuatBanRoutes'));
+app.use('/api/docgia', require('./routes/docGiaRoutes')); 
+app.use('/api/sach', require('./routes/sachRoutes'));       
+app.use('/api/nhanvien', require('./routes/nhanVienRoutes')); 
+app.use('/api/nhaxuatban', require('./routes/nhaXuatBanRoutes')); 
 
 app.get('/', (req, res) => {
     res.send('UniBorrow API is running...');
@@ -26,6 +26,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server đang chạy tại cổng: ${PORT}`); 
     swaggerDocs(app, PORT);
+    initCronJobs();
 });
